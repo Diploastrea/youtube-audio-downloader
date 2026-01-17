261aae0a965e2f2a1f0fa3f28891a9c2546653b8
Viet Duc Ho <23vietduc@gmail.com>
2026-01-17 23:46:56 +1100

    Initial commit

---
+++ src/main/java/com/yt/dl/ytaudiodownloader/YtAudioDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.yt.dl.ytaudiodownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtAudioDownloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtAudioDownloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yt/dl/ytaudiodownloader/YtAudioDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.dl.ytaudiodownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtAudioDownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
